c75d6681d2d56a65b19f8eb91e1f3956623a9b15
Mikel8Lee <connerrobert927@gmail.com>
2021-06-30 17:41:50 +0530

    Updated login and register page

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -1,12 +1,14 @@
         // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
         (function () {
             'use strict'
 
             // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            const forms = document.querySelectorAll('.validated-form')
+            var forms = document.querySelectorAll('.validated-form')
 
             // Loop over them and prevent submission
-            Array.from(forms)
+            Array.prototype.slice.call(forms)
                 .forEach(function (form) {
                     form.addEventListener('submit', function (event) {
                         if (!form.checkValidity()) {
@@ -17,5 +19,4 @@
                         form.classList.add('was-validated')
                     }, false)
                 })
-        })
-
+        })()
