8fc9a0f03201d7fda0dba8917744005f4af0fde0
Mikel8Lee <connerrobert927@gmail.com>
2021-06-30 23:53:48 +0530

    Added start script

--- app.js
+++ app.js
@@ -208,4 +208,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(3000, () => console.log('Serving on Port 3000'));
+//Defining our Port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Serving on Port ${port}`));
